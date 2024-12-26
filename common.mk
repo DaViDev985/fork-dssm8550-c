e61670a26635b0f2aa17a8027ea3c02177ce2e6a
chandu078 <chandudyavanapelli03@gmail.com>
2024-12-26 15:12:39 +0530

    sm8550-common: Remove manual build of libtinycompress and libavservices_minijail
    These libraries are automatically built as part of shared_libs.
    
    Change-Id: Ia7ddfacca4bfcdd7984fe93cc9d61a3791f77ddd
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -39,7 +39,6 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libsndcardparser \
-    libtinycompress \
     libvolumelistener \
     sound_trigger.primary.kalama
 
@@ -187,7 +186,6 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libavservices_minijail \
     libpalclient
 
 PRODUCT_COPY_FILES += \
